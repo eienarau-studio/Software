26de8c6bddb0157eaf0dc0602a98849f73e3cd1c
amovfx <amo.vfx@protonmail.com>
2019-03-06 11:53:22 -0800

    removed tabs from YAML string. flattenDict fn constructs relative paths for directorys from YAML build directories now builds dirs from a list of relative paths.

--- BuildStudio/buildStudio.py
+++ BuildStudio/buildStudio.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python2.7
 
+"""
+Builds directory structure for studio
+"""
+
 import yaml
 import os
 
@@ -9,8 +13,8 @@ STUDIOPATH = os.path.join(HOME, STUDIONAME)
 
 YAML="""Projects :
 Software :
-	python2.7 :
-	python3.0 :
+    python2.7 :
+    python3.0 :
 DCC :"""
 #dump YAML for example
 
@@ -28,20 +32,52 @@ def getConfig():
 
     return d_yaml
 
-def buildDirectories(dictOfDict):
-    for k, v in dictOfDict.items():
-        if isinstance(v, dict):
-            buildDirectories(v)
+def buildDirectories(relativePaths):
+    """
+    Builds directorys from a a list.
+    :param relativePaths:
+        a list of relative paths
 
-        newdir = os.path.join(STUDIOPATH, k)
+    """
+
+
+    for relPath in relativePaths:
+        newdir = os.path.join(STUDIOPATH, relPath)
         if not os.path.exists(newdir):
+            print ("Creating directory: {}".format(newdir))
             os.makedirs(newdir)
+        else:
+            print ("Dir: {} exists".format(newdir))
+
+
+def flattenDictKeys(d):
+    """
+
+    :param d:
+        dictionary preferable of a yaml config file
+    :return:
+        a flattened relative directory structure of the YAML configs keys.
+    """
+    results = []
+
+    def visit(subdict, results, parent=None ):
+        for k, v in subdict.items():
+            newKey = k if parent is None else os.path.join(parent, k)
+            if isinstance(v, dict):
+                visit(v, results, parent=newKey)
+            else:
+                results.append(newKey)
+
+    visit(d, results)
+    return results
+
 
 
 if __name__ == "__main__":
 
-    configObj = getConfig()
-    buildDirectories(configObj)
+    configDict = getConfig()
+    paths = flattenDictKeys(configDict)
+    buildDirectories(paths)
 
 
 
